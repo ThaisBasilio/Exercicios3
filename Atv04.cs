43ac78d6f8cb0457caa8b4c1401185e7d2877922
ThaisBasilio <0001272406@senaimgaluno.com.br>
2026-07-06 17:28:11 -0300

    Exercicios

---
+++ Atv04.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lista 3
+{
+    class Atv04
+    {
+        static void Main()
+        {
+            int[] vetor = new int[8];
+            Console.WriteLine("")
+        }
+        
+    }
+}
