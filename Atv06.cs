33fcf686e4ab25d1ba28a387d4a86ae84878313e
ThaisBasilio <0001272406@senaimgaluno.com.br>
2026-07-08 14:21:21 -0300

    Exercicio da Aula

---
+++ Atv06.cs
@@ -0,0 +1,23 @@
+using System;
+  class AtvAula
+    { 
+        static void Main()
+        {
+            int[] vetor = new int[10];
+            Console.WriteLine("Digite 10 valores inteiros do vetor: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Posição: {i}: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            int maior = vetor[0];
+            int menor = vetor[0];
+            for (int i = 1; i < 10; i++)
+            {
+                if (vetor[i] > maior) {maior = vetor[i];}
+                if (vetor[i] < menor) {menor = vetor[i];}
+            }
+            Console.WriteLine($"Maior valor: {maior}");
+            Console.WriteLine($"Menor valor: {menor}");
+        }
+    }
